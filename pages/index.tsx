63227ea31abc0849e301f47a77ea066df1180d08
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-01-07 00:32:31 +0530

    updated the add tweet modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -149,37 +149,39 @@ export default function Home() {
         </div>
         <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-y-scroll no-scrollbar border-gray-600">
           <div>
-            <div className="grid grid-cols-12 gap-3">
-              <div className="col-span-1">
-                {user?.profileImageURL && (
-                  <Image
-                    className="rounded-full"
-                    src={user?.profileImageURL}
-                    alt="user-img"
-                    height={50}
-                    width={50}
-                  />
-                )}
-              </div>
-              <div className="col-span-11">
-                <textarea
-                  value={content}
-                  onChange={(e) => setContent(e.target.value)}
-                  className="w-full bg-transparent text-xl px-3 border-b border-slate-700"
-                  rows={3}
-                  placeholder="What's happening?"
-                ></textarea>
-                <div className="mt-2">
-                  <BiImageAlt
-                    onClick={handleSelectImage}
-                    className="text-xl flex justify-between items-center"
-                  />
-                  <button
-                    onClick={handleCreateTweet}
-                    className="bg-[#1d9bf0] font-semibold text-sm py-2 px-4 rounded-full"
-                  >
-                    Chirp
-                  </button>
+            <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+              <div className="grid grid-cols-12 gap-3">
+                <div className="col-span-1">
+                  {user?.profileImageURL && (
+                    <Image
+                      className="rounded-full"
+                      src={user?.profileImageURL}
+                      alt="user-img"
+                      height={50}
+                      width={50}
+                    />
+                  )}
+                </div>
+                <div className="col-span-11">
+                  <textarea
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    className="w-full bg-transparent text-xl px-3 border-b border-slate-700 no-scrollbar"
+                    rows={3}
+                    placeholder="What's happening?"
+                  ></textarea>
+                  <div className="mt-2  flex justify-between items-center">
+                    <BiImageAlt
+                      onClick={handleSelectImage}
+                      className="text-xl"
+                    />
+                    <button
+                      onClick={handleCreateTweet}
+                      className="bg-[#1d9bf0] font-semibold text-sm py-2 px-4 rounded-full"
+                    >
+                      Chirp
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
